dd53342a58ebcea0963e892130eb5a7f5fbda943
Matt Johnson <mattajohnson26@gmail.com>
2024-09-04 14:35:07 -0600

    added seeds file and new dependency

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,28 @@
-DROP DATABASE IF EXISTS movies_db;
+DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
-\c movies_db;
+\c company_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id SERIAL PRIMARY KEY,
   department_name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(30) UNIQUE NOT NULL
-    salary DECIMAL NOT NULL
-    department(id)
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
+    FOREIGN KEY (department_id),
+    REFERENCES department(id)
 );
+
+CREATE TABLE employees (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  FOREIGN KEY (role_id),
+  REFERENCES role(id),
+
+)
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO departments ()
+VALUES
+
+INSERT INTO roles ()
+VALUES
+
+INSERT INTO employees ()
+VALUES
